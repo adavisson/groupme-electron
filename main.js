59d1c6e71c19380179e2f417a8dd0cb9ce49b381
Andrew Davisson <akdavisson4@gmail.com>
2020-09-17 14:56:13 -0600

    copied from gmail-electron

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, session, Notification } = require('electron')
+const os = require('os')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1300,
+    height: 900,
+    title: 'Gmail',
+  })
+
+  win.loadURL('https://www.groupme.com')
+  win.webContents.on('new-window', (e, url) => {
+    e.preventDefault()
+    require('electron').shell.openExternal(url)
+  })
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
